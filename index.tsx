429f6b7dca613cb1f03e35d8c1cc5cd6719024ef
NinjaWorld1234 <d.naser76@gmail.com>
2025-12-07 13:29:03 +0200

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -18,12 +18,11 @@ if ('serviceWorker' in navigator) {
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
